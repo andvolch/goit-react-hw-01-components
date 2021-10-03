fd9b7ff96e3308cf2e2a9606f4658b8981784ce5
Andrey Volchenskiy <79094272+andvolch@users.noreply.github.com>
2021-10-03 20:06:12 +0300

    test

---
+++ src/components/FriendList/FriendList.js
@@ -0,0 +1,9 @@
+export default function FriendList({ friends }) {
+  return (
+    <li className="item">
+      <span className="status"></span>
+      <img className="avatar" src="" alt="" width="48" />
+      <p className="name"></p>
+    </li>
+  );
+}
